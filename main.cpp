3c7ef61e883c752312d1019e447ef23370aadefa
Daniel Lopes <danielorkae@outlook.com>
2019-09-08 16:46:54 -0300

    main menu

--- main.cpp
+++ main.cpp
@@ -6,35 +6,166 @@
 using namespace std;
 
 vector<Concessionaria *> concessionarias;
+Concessionaria *concessionariaSelecionada = NULL;
 
+int menuPrincipal();
+int menuDaConcessionaria();
 void criarConcessionaria();
+Concessionaria *escolherConcessionaria();
 Concessionaria *encontrarConcessionaria(string cnpj, bool imprimir);
 
 int main()
 {
+  bool sair = false;
+  int selecaoMenuPrincipal, selecaoMenuConcessionaria;
 
-  return 0;
+  while (!sair)
+  {
+    if (concessionariaSelecionada == NULL)
+    {
+      selecaoMenuPrincipal = menuPrincipal();
+
+      switch (selecaoMenuPrincipal)
+      {
+      case 1:
+        concessionariaSelecionada = escolherConcessionaria();
+        break;
+      case 2:
+        criarConcessionaria();
+        break;
+      default:
+        return 0;
+      }
+    }
+    else
+    {
+      selecaoMenuConcessionaria = menuDaConcessionaria();
+
+      switch (selecaoMenuConcessionaria)
+      {
+      case 1:
+        concessionariaSelecionada->listarEstoque();
+        break;
+      case 2:
+        concessionariaSelecionada->adicionarAutomovelAoEstoque();
+        break;
+      case 3:
+        concessionariaSelecionada->aumentarValorDoEstoque();
+        break;
+      default:
+        concessionariaSelecionada = NULL;
+        break;
+      }
+    }
+  }
+
+  return 1;
+}
+
+int menuPrincipal()
+{
+  int selecao;
+
+  cout
+      << endl
+      << "=== MENU PRINCIPAL ===" << endl
+      << "1 - Escolher concessionária" << endl
+      << "2 - Cadastrar concessionária" << endl
+      << "0 - Sair" << endl;
+
+  cin >> selecao;
+
+  if (selecao < 0 || selecao > 3)
+  {
+    return menuPrincipal();
+  }
+
+  return selecao;
+}
+
+int menuDaConcessionaria()
+{
+  int selecao;
+
+  cout
+      << endl
+      << "=== MENU DA CONCESSIONÁRIA ===" << endl
+      << "1 - Listar automóveis" << endl
+      << "2 - Cadastrar automóvel" << endl
+      << "3 - Aumentar preço de todos automóveis" << endl
+      << "0 - Voltar ao menu principal" << endl;
+
+  cin >> selecao;
+
+  if (selecao < 0 || selecao > 3)
+  {
+    return menuDaConcessionaria();
+  }
+
+  return selecao;
+}
+
+Concessionaria *escolherConcessionaria()
+{
+  unsigned int selecao;
+
+  cout << endl
+       << "=== ESCOLHER CONCESSIONÁRIA ===" << endl;
+
+  for (unsigned int i = 0; i < concessionarias.size(); i++)
+  {
+    cout << i + 1 << " - " << concessionarias[i]->getNome() << endl;
+  }
+
+  if (concessionarias.size() == 0)
+  {
+    cout << "Nenhuma concessionária cadastrada." << endl
+         << endl;
+
+    return NULL;
+  }
+
+  cout << "0 - Voltar" << endl;
+
+  cin >> selecao;
+
+  if (selecao == 0)
+  {
+    return NULL;
+  }
+
+  if (selecao < 0 || selecao > concessionarias.size())
+  {
+    return escolherConcessionaria();
+  }
+
+  return concessionarias[selecao - 1];
 }
 
 void criarConcessionaria()
 {
   string nome, cnpj;
 
-  cout << "=== CADASTRO DE CONCESSIONáRIA ===" << endl;
+  cout << endl
+       << "=== CADASTRO DE CONCESSIONÁRIA ===" << endl;
 
-  cout << "CNPJ: " << endl;
+  cout << "CNPJ: ";
   cin >> cnpj;
 
   if (encontrarConcessionaria(cnpj, false) != NULL)
   {
     cout << "Esta concessionária já está cadastrada." << endl;
-    return;
   }
+  else
+  {
+    cout << "Nome: ";
+    cin >> nome;
 
-  cout << "Nome: " << endl;
-  cin >> nome;
+    concessionarias.push_back(new Concessionaria(nome, cnpj));
 
-  concessionarias.push_back(new Concessionaria(nome, cnpj));
+    cout << endl
+         << "Concessionária cadastrada com sucesso!" << endl;
+  }
 }
 
 Concessionaria *encontrarConcessionaria(string cnpj, bool imprimir)
@@ -47,10 +178,7 @@ Concessionaria *encontrarConcessionaria(string cnpj, bool imprimir)
     {
       if (imprimir)
       {
-        cout
-            << "Concessionária " << concessionaria->getNome() << endl
-            << "CNPJ: \t" << concessionaria->getCnpj() << endl
-            << "Estoque: " << concessionaria->getQntEstoque() << endl;
+        cout << concessionaria;
       }
 
       return concessionaria;
